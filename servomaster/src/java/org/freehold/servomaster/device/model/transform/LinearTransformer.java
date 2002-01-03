e1d44780fdf7d8874f2a318e5493e83e8e7f5385
vtt <vtt@9b91227f-e13f-0410-a8b0-d07aa902b8ad>
2002-01-03 22:10:01 +0000

    Documentation update

--- servomaster/src/java/org/freehold/servomaster/device/model/transform/LinearTransformer.java
+++ servomaster/src/java/org/freehold/servomaster/device/model/transform/LinearTransformer.java
@@ -18,14 +18,48 @@ import org.freehold.servomaster.device.model.Servo;
  * cumbersome, plus adds unnecessary overhead, so it was decided to provide
  * a separate implementation.
  *
+ * <p>
+ *
+ * <strong>NOTE:</strong>
+ *
+ * In the current form, the linear transformer supports only 0\u00B0 to
+ * 180\u00B0 transformation. However, it will be fixed soon.
+ *
  * @author Copyright &copy; <a href="mailto:vt@freehold.crocodile.org">Vadim Tkachenko</a> 2001
- * @version $Id: LinearTransformer.java,v 1.2 2002-01-01 01:42:13 vtt Exp $
+ * @version $Id: LinearTransformer.java,v 1.3 2002-01-03 22:10:01 vtt Exp $
  */
 public class LinearTransformer extends AbstractCoordinateTransformer {
 
+    /**
+     * Create an instance supporting 0\u00B0 to 180\u00B0 linear
+     * transformation.
+     *
+     * @param target Servo to provide the transformation for.
+     */
     public LinearTransformer(Servo target) {
     
         super(target);
+    }
+    
+    /**
+     * Create an instance supporting a linear transformation on arbitrary
+     * angle range.
+     *
+     * @param target Servo to provide the transformation for.
+     *
+     * @param startAngle Starting rotational angle, degrees.
+     *
+     * @param endAngle Ending rotational angled, degrees.
+     *
+     * @exception IllegalArgumentException if the ending angle is less than
+     * starting angle, or either of them is outside of 0...180 range - it
+     * doesn't make sense to support that for a servo.
+     */
+    public LinearTransformer(Servo target, double startAngle, double endAngle) {
+    
+        super(target);
+        
+        throw new Error("Not Implemented");
     }
     
     protected double transform(double value) {

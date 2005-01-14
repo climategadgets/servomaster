0c31e9956c72cef31b26a0a7e11e5d2fb3bbd135
vtt <vtt@9b91227f-e13f-0410-a8b0-d07aa902b8ad>
2005-01-14 01:07:29 +0000

    Checkpoint: refactoring to remove redundant code

--- trunk/servomaster/src/java/org/freehold/servomaster/device/impl/pololu/Serial16ServoController.java
+++ trunk/servomaster/src/java/org/freehold/servomaster/device/impl/pololu/Serial16ServoController.java
@@ -1,5 +1,43 @@
 package org.freehold.servomaster.device.impl.pololu;
 
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.freehold.servomaster.device.impl.serial.AbstractSerialServoController;
+import org.freehold.servomaster.device.model.Servo;
+import org.freehold.servomaster.device.model.silencer.SilentProxy;
+
 public class Serial16ServoController extends AbstractSerialServoController {
 
+    /**
+     * {@inheritDoc}
+     */
+    public void reset() throws IOException {
+    
+        System.err.println("reset(): not implemented");
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected SilentProxy createSilentProxy() {
+    
+        throw new Error("Not Implemented");
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public int getServoCount() {
+    
+        return 16;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public synchronized Servo getServo(String id) throws IOException {
+    
+        throw new Error("Not Implemented");
+    }
 }

cadef96f42dda12809d79b6e9bf801ff51741e19
vtt <vtt@9b91227f-e13f-0410-a8b0-d07aa902b8ad>
2005-01-14 03:27:40 +0000

    Documentation update

--- servomaster/src/java/org/freehold/servomaster/device/impl/pololu/Serial16ServoController.java
+++ servomaster/src/java/org/freehold/servomaster/device/impl/pololu/Serial16ServoController.java
@@ -7,6 +7,18 @@ import org.freehold.servomaster.device.impl.serial.AbstractSerialServoController
 import org.freehold.servomaster.device.model.Servo;
 import org.freehold.servomaster.device.model.silencer.SilentProxy;
 
+/**
+ * <a href="http://pololu.com/products/pololu/0240/" target="_top">Pololu
+ * Serial 16-Servo Controller</a> controller.
+ *
+ * Should also work with the <a
+ * href="http://www.pololu.com/products/pololu/0290/"
+ * target="_top">8-Servo</a> controller, but be careful not to access servos
+ * with IDs of 8 and up.
+ *
+ * @author Copyright &copy; <a href="mailto:vt@freehold.crocodile.org">Vadim Tkachenko</a> 2005
+ * @version $Id: Serial16ServoController.java,v 1.4 2005-01-14 03:27:40 vtt Exp $
+ */
 public class Serial16ServoController extends AbstractSerialServoController {
 
     /**

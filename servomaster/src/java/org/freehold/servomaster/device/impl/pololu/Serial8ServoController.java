85d3f286e9827aafb1a78e31fc0c1b4c5ebedf0c
vtt <vtt@9b91227f-e13f-0410-a8b0-d07aa902b8ad>
2005-05-12 21:36:37 +0000

    - Moved logic common for all Pololu controllers into a common superclass; - Created a separate class for 8-Servo serial controller.

--- servomaster/src/java/org/freehold/servomaster/device/impl/pololu/Serial8ServoController.java
+++ servomaster/src/java/org/freehold/servomaster/device/impl/pololu/Serial8ServoController.java
@@ -10,25 +10,20 @@ import org.freehold.servomaster.device.model.ServoController;
 import org.freehold.servomaster.device.model.silencer.SilentProxy;
 
 /**
- * <a href="http://pololu.com/products/pololu/0240/" target="_top">Pololu
- * Serial 16-Servo Controller</a> controller.
- *
- * Should also work with the <a
- * href="http://www.pololu.com/products/pololu/0290/"
- * target="_top">8-Servo</a> controller, but be careful not to access servos
- * with IDs of 8 and up.
+ * <a href="http://pololu.com/products/pololu/0290/" target="_top">Pololu
+ * Serial 8-Servo Controller</a> controller.
  *
  * @author Copyright &copy; <a href="mailto:vt@freehold.crocodile.org">Vadim Tkachenko</a> 2005
- * @version $Id: Serial16ServoController.java,v 1.15 2005-05-12 21:36:37 vtt Exp $
+ * @version $Id: Serial8ServoController.java,v 1.1 2005-05-12 21:36:37 vtt Exp $
  */
-public class Serial16ServoController extends PololuSerialServoController {
+public class Serial8ServoController extends PololuSerialServoController {
 
-    public Serial16ServoController() {
+    public Serial8ServoController() {
     
         // Can't invoke this(null) because this will blow up in doInit()
     }
     
-    public Serial16ServoController(String portName) throws IOException {
+    public Serial8ServoController(String portName) throws IOException {
     
         super(portName);
     }
@@ -38,22 +33,22 @@ public class Serial16ServoController extends PololuSerialServoController {
      */
     public int getServoCount() {
     
-        return 16;
+        return 8;
     }
-    
+
     /**
      * {@inheritDoc}
      */
     protected final Meta createMeta() {
     
-        return new Serial16Meta();
+        return new Serial8Meta();
     }
 
-    protected class Serial16Meta extends PololuMeta {
+    protected class Serial8Meta extends PololuMeta {
     
-        public Serial16Meta() {
+        public Serial8Meta() {
         
-            properties.put("manufacturer/model", "Serial 16-Servo");
+            properties.put("manufacturer/model", "Serial 8-Servo");
         }
     }
 }

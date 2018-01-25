49d60930ecfd05afbadca3c8fa93a337fd6a3831
Vadim Tkachenko <vt@homeclimatecontrol.com>
2018-01-24 23:34:06 -0700

    Replaced a controller-specific control with reusable.

--- servomaster-common/src/main/java/net/sf/servomaster/device/impl/debug/NullServoControllerView.java
+++ servomaster-common/src/main/java/net/sf/servomaster/device/impl/debug/NullServoControllerView.java
@@ -24,6 +24,7 @@ import net.sf.servomaster.device.model.Servo;
 import net.sf.servomaster.device.model.ServoController;
 import net.sf.servomaster.device.model.ServoControllerListener;
 import net.sf.servomaster.view.ServoControllerView;
+import net.sf.servomaster.view.ServoRangeControl;
 
 /**
  * This class renders and allows to control the features specific to {@link NullServoController}.
@@ -61,7 +62,7 @@ public class NullServoControllerView extends JPanel implements ServoControllerLi
 
             for ( Iterator<Servo> i = servos.iterator(); i.hasNext(); ) {
 
-                add(new ServoPanel(i.next()));
+                add(new ServoRangeControl(i.next()));
             }
 
         } catch ( IOException ioex ) {
@@ -80,181 +81,6 @@ public class NullServoControllerView extends JPanel implements ServoControllerLi
         // FIXME
 
         //logger.debug("Silent: " + mode);
-    }
-
-    @SuppressWarnings("serial")
-    private class ServoPanel extends JPanel implements ChangeListener, ItemListener {
-
-        private Servo servo;
-        private JCheckBox silentBox;
-        private JLabel silentLabel;
-
-        private JLabel rangeLabel;
-        private JSlider minSlider;
-        private JSlider maxSlider;
-
-        private String units;
-        private int defaultRangeMin;
-        private int defaultRangeMax;
-        private int min;
-        private int max;
-
-        ServoPanel(Servo servo) {
-
-            this.servo = servo;
-
-            setBorder(BorderFactory.createTitledBorder("ID: " + servo.getName()));
-
-            GridBagLayout layout = new GridBagLayout();
-            GridBagConstraints cs = new GridBagConstraints();
-
-            setLayout(layout);
-
-            cs.fill = GridBagConstraints.HORIZONTAL;
-            cs.gridx = 0;
-            cs.gridy = 0;
-            cs.gridwidth = 2;
-            cs.gridheight = 1;
-            cs.weightx = 1;
-            cs.weighty = 0;
-
-            // The servo range now can be calculated based on the metadata
-            // provided by the servo itself. Note that in this case we a)
-            // know that the controller supports this particular metadata b)
-            // we're taking the controller meta, not the servo meta, 'cause
-            // we need the defaults.
-
-            Meta meta = servo.getController().getMeta();
-            units = meta.getProperty("servo/range/units").toString();
-
-            String min = meta.getProperty("servo/range/min").toString();
-            String max = meta.getProperty("servo/range/max").toString();
-
-            defaultRangeMin = Integer.parseInt(min);
-            defaultRangeMax = Integer.parseInt(max);
-
-            this.min = defaultRangeMin;
-            this.max = defaultRangeMax;
-
-            rangeLabel = new JLabel(renderRange(defaultRangeMin, defaultRangeMax), JLabel.CENTER);
-            rangeLabel.setToolTipText("Current minimum/maximum servo position");
-            rangeLabel.setBorder(BorderFactory.createTitledBorder("Range, " + units));
-
-            layout.setConstraints(rangeLabel, cs);
-            add(rangeLabel);
-
-            cs.gridy++;
-            cs.gridwidth = 1;
-            cs.gridheight = 2;
-
-            cs.fill = GridBagConstraints.BOTH;
-            cs.weighty = 1;
-
-            // Now that we have the metadata, we can properly calculate the
-            // minimum and maximum slider positions. Let's suppose that the
-            // default servo controller setup is OK for most servos, and
-            // allow 25% edge outside the default range. This is a demo
-            // console, all in all, so the exact range doesn't really
-            // matter.
-
-            // VT: NOTE: What it does matter for, though, is determining the
-            // physical range of each particular servo.
-
-            int delta = (defaultRangeMax - defaultRangeMin) / 4;
-
-            // Same goes for ticks.
-
-            int majorTicks = delta;
-            int minorTicks = majorTicks / 10;
-
-            minSlider = new JSlider(JSlider.VERTICAL, defaultRangeMin - delta, defaultRangeMin + delta, defaultRangeMin);
-            minSlider.setToolTipText("Move this to set the minimum servo position");
-            minSlider.setMajorTickSpacing(majorTicks);
-            minSlider.setMinorTickSpacing(minorTicks);
-            minSlider.setPaintTicks(true);
-            minSlider.setPaintLabels(true);
-            minSlider.setSnapToTicks(false);
-
-            layout.setConstraints(minSlider, cs);
-            add(minSlider);
-
-            cs.gridx = 1;
-
-            maxSlider = new JSlider(JSlider.VERTICAL, defaultRangeMax - delta, defaultRangeMax + delta, defaultRangeMax);
-            maxSlider.setToolTipText("Move this to set the maximum servo position");
-            maxSlider.setMajorTickSpacing(majorTicks);
-            maxSlider.setMinorTickSpacing(minorTicks);
-            maxSlider.setPaintTicks(true);
-            maxSlider.setPaintLabels(true);
-            maxSlider.setSnapToTicks(false);
-
-            layout.setConstraints(maxSlider, cs);
-            add(maxSlider);
-
-            minSlider.addChangeListener(this);
-            maxSlider.addChangeListener(this);
-
-        }
-
-        @Override
-        public void stateChanged(ChangeEvent e) {
-
-            Object source = e.getSource();
-
-            if ( source == minSlider ) {
-
-                min = minSlider.getValue();
-
-                servo.getMeta().setProperty("servo/range/min", Integer.toString(min));
-                rangeLabel.setText(renderRange(min, max));
-
-            } else if ( source == maxSlider ) {
-
-                max = maxSlider.getValue();
-
-                servo.getMeta().setProperty("servo/range/max", Integer.toString(max));
-                rangeLabel.setText(renderRange(min, max));
-            }
-        }
-
-        private String renderRange(int min, int max) {
-
-            StringBuilder sb = new StringBuilder();
-
-            sb.append(Integer.toString(min));
-            sb.append(" - ");
-            sb.append(Integer.toString(max));
-
-            return sb.toString();
-        }
-
-        /**
-         * React to checkbox status changes.
-         */
-        @Override
-        public void itemStateChanged(ItemEvent e) {
-
-            if ( e.getSource() == silentBox ) {
-
-                boolean selected = (e.getStateChange() == ItemEvent.SELECTED);
-
-                /*
-
-                // VT: FIXME: servo silent support has to be implemented first
-
-                try {
-
-                    servo.setSilentMode(selected);
-
-                } catch ( IOException ioex ) {
-
-                    logger.warn("Unhandled exception", ioex);
-                }
-                 */
-
-                // VT: FIXME: Reflect the changes on the silentLabel
-            }
-        }
     }
 
     @Override
